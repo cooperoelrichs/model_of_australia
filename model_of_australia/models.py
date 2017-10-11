Fixed the International Shared Variance Model.

model_of_australia/models.py
@@ -254,17 +254,19 @@ def international_shared_variance_model_fn(
     Y, iters, tune_iters
 ):
     with pm.Model() as model:
-        MU = pm.Normal('mu', 0, 1e6, shape=Y.shape[1])
+        # MU = pm.Normal('mu', 0, 1e6, shape=Y.shape[1])
         sd = pm.InverseGamma('sd', alpha=1, beta=1)
 
         Y_hat = [
-            pm.Normal('y%i' % i, MU[i], sd, observed=Y[:, i][np.isfinite(Y[:, i])])
+            # pm.Normal('y%i' % i, MU[i], sd, observed=Y[:, i][np.isfinite(Y[:, i])])
+            pm.Normal('y%i' % i, 0, sd, observed=Y[:, i][np.isfinite(Y[:, i])])
             for i in range(Y.shape[1])
         ]
 
         step = pm.Metropolis()
         trace = pm.sample(
             int(iters),
+            trace=[a for a in Y_hat] + [sd],
             tune=int(tune_iters),
             step=step,
             njobs=4,

model_of_australia/simulators.py
@@ -78,7 +78,8 @@ class SharedVarianceInternationalGDPSimulator():
     def simulate(init_gdp, n_years, n_iter, params):
         gdp_sim = np.empty((n_iter, n_years))
         for i in range(n_iter):
-            mu = params['mu_australia'][0][2].rvs(*params['mu_australia'][0][1])
+            # mu = params['mu_australia'][0][2].rvs(*params['mu_australia'][0][1])
+            mu = params['y_australia'][0][1][0]
             sd = params['sd'][0][2].rvs(*params['sd'][0][1])
             deltas = stats.norm.rvs(mu, sd, size=n_years)
 

scripts/fit_models.py
@@ -42,16 +42,27 @@ def fit_correlated_sectors_model():
     return correlated_sectors_model
 
 def fit_international_shared_variance_model():
+    Y = load_un_gdp_pc_d()[1].values
     international_shared_variance_model = ModelContainer(
         name='International Shared Variance Model',
         folder='international_shared_variance_model',
         model_fn=international_shared_variance_model_fn,
-        data=load_un_gdp_pc_d()[1].values,
-        parameter_spec=[(1, 'mu', stats.norm), (0, 'sd', stats.invgamma)],
+        data=Y,
+        parameter_spec=[
+            (0, 'y%i' % i, stats.norm) for i in range(Y.shape[1])
+        ] + [(0, 'sd', stats.invgamma)],
         outputs_dir=settings.OUTPUTS_DIR
     )
-    international_shared_variance_model.run()
-    parameters = extract_mu_for_australia(
+    # international_shared_variance_model.run()
+    international_shared_variance_model.results = international_shared_variance_model.fit_model()
+    international_shared_variance_model.parameters = international_shared_variance_model.calculate_parameters()
+    ModelContainer.save_parameters(
+        international_shared_variance_model.parameters,
+        international_shared_variance_model.folder,
+        international_shared_variance_model.outputs_dir
+    )
+
+    parameters = extract_dist_for_australia(
         international_shared_variance_model.parameters,
         load_un_gdp_pc_d()[1],
     )
@@ -61,6 +72,11 @@ def fit_international_shared_variance_model():
     )
     international_shared_variance_model.parameters = parameters
     return international_shared_variance_model
+
+def extract_dist_for_australia(parameters, data):
+    aust_i = np.where(data.columns == 'Australia')[0][0]
+    parameters['y_australia'] = parameters['y%i' % aust_i]
+    return parameters
 
 def extract_mu_for_australia(parameters, data):
     parameters['mu_australia'] = [